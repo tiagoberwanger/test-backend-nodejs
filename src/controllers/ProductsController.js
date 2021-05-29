e3f976de184d6cce81a1a54f0c4e56a38d8d5056
Tiago Berwanger <berwangertiago@gmail.com>
2021-05-29 17:42:06 -0300

    moving status codes to utils directory

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -1,5 +1,4 @@
-const { Router, query } = require('express');
-const connection = require('../models/connection');
+const { Router } = require('express');
 const { 
   create,
   edit,
@@ -8,11 +7,7 @@ const {
   filter
 } = require('../services/ProductsService');
 
-const { filterProduct } = require('../models/ProductsModel')
-
-const STATUS_OK = 200;
-const STATUS_CREATED = 201;
-const STATUS_UNPROCESSABLE= 422;
+const { OK, CREATED, UNPROCESSABLE } = require('../utils/StatusCodes')
 
 const route = new Router();
 
@@ -20,8 +15,8 @@ route.post('/', async (req, res) => {
   const { title, description, price, category } = req.body;  
   const created = await create(title, description, price, category);
   
-  if (!create) res.status(STATUS_UNPROCESSABLE).json({ message: 'Unable to create product' });
-  return res.status(STATUS_CREATED).json(created);
+  if (!create) res.status(UNPROCESSABLE).json({ message: 'Unable to create product' });
+  return res.status(CREATED).json(created);
 });
 
 route.put('/:id', async (req, res) => {
@@ -29,28 +24,28 @@ route.put('/:id', async (req, res) => {
   const { title, description, price, category } = req.body;
   const updated = await edit(id, title, description, price, category);
 
-  if (!updated) res.status(STATUS_UNPROCESSABLE).json({ message: 'Unable to update product'});
-  return res.status(STATUS_OK).json({ message: `Product ${id} updated successfully`});
+  if (!updated) res.status(UNPROCESSABLE).json({ message: 'Unable to update product'});
+  return res.status(OK).json({ message: `Product ${id} updated successfully`});
 });
 
 route.get('/', async (_req, res) => {
   const allProducts = await getAll();
-  return res.status(STATUS_OK).json(allProducts);
+  return res.status(OK).json(allProducts);
 });
 
 route.delete('/:id', async (req, res) => {
   const { id } = req.params;
   const deleted = await exclude(id);
 
-  if (!deleted) return res.status(STATUS_UNPROCESSABLE).json({ message: 'Unable to delete product'});
-  return res.status(STATUS_OK).json({ message: `Product ${id} deleted successfully`});
+  if (!deleted) return res.status(UNPROCESSABLE).json({ message: 'Unable to delete product'});
+  return res.status(OK).json({ message: `Product ${id} deleted successfully`});
 });
 
 route.get('/search', async (req, res) => {
   const { q } = req.query;
   const filtered = await filter(q)
   console.log(filtered)
-  return res.status(STATUS_OK).json(filtered);
+  return res.status(OK).json(filtered);
 })
 
 module.exports = route;
---
+++ src/utils/StatusCodes.js
@@ -0,0 +1,7 @@
+const status = {
+  OK: 200,
+  CREATED: 201,
+  UNPROCESSABLE: 422,
+}
+
+module.exports = status;
