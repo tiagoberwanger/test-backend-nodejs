045cff811417e3013a55107f5f2048bcb8749c31
Tiago Berwanger <berwangertiago@gmail.com>
2021-05-29 14:38:10 -0300

    updateProduct function with mongodb

--- src/models/ProductsModel.js
+++ src/models/ProductsModel.js
@@ -2,15 +2,25 @@ const connection = require('./connection');
 const { ObjectId } = require('mongodb');
 
 const createProduct = async (title, description, price, category) => {
-    const { insertedId } = await connection()
-      .then((db) => db.collection('catalog').insertOne({ 
-        title, description, price, category 
-      }));
-    return ({
-      _id: insertedId, title, description, price, category
-    });
-  };
+  const { insertedId } = await connection()
+    .then((db) => db.collection('catalog').insertOne({ 
+      title, description, price, category 
+    }));
+  return ({
+    _id: insertedId, title, description, price, category
+  });
+};
+
+const updateProduct = async (id, category) => {
+  const updatedProduct = await connection()
+    .then((db) => db.collection('catalog').updateOne(
+      db.collection('catalog').findOne({_id: ObjectId(id) }),
+      {$set:{ category }},));
+  return updatedProduct;
+};
+
 
   module.exports = {
     createProduct,
+    updateProduct
   };
